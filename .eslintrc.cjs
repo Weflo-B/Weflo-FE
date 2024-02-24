b14d6200dee339a76d10a5d3ec8dcc689c565421
aaminha <dks4857@gmail.com>
2024-02-25 03:34:16 +0900

    chore: eslint 적용 안되는 문제 해결 - eslint-plugin-prettier 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,10 @@
-/* module.exports = {
+module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier',
@@ -16,64 +15,6 @@
   plugins: ['react-refresh', 'react', 'prettier', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@/**',
-            group: 'internal',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react', 'react-dom'],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc' },
-      },
-    ],
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-  },
-  settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
-      version: 'detect',
-    },
-    // Tells eslint how to resolve imports
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-};
- */
-
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-    'prettier',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react', 'prettier', '@typescript-eslint', 'import'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
     'import/order': [
@@ -98,20 +39,9 @@ module.exports = {
       },
     ],
   },
-  /* settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  }, */
   settings: {
     'import/resolver': {
-      alias: {
-        map: [
-          ['@', path.resolve(__dirname, 'src')],
-          // 다른 별칭들도 필요한 경우 여기에 추가
-        ],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      },
+      typescript: {},
     },
   },
 };
