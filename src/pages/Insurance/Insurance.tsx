f8a7813dabb43ea47a243824b410ece4cc1b79e9
hyunn522 <tjgus52213@gmail.com>
2024-03-04 20:05:06 +0900

    feat: 보험 헤더 레이아웃 작성 - leftChevron svg 파일 임포트

--- src/pages/Insurance/Insurance.tsx
+++ src/pages/Insurance/Insurance.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import styles from '@/pages/Insurance/Insurance.module.scss';
+import LeftChevron from '@/assets/icons/leftChevron.svg';
+import styles from '@/pages/Insurance/Insurance.module.scss';
 
 const Insurance = () => {
-  return <div>Insurance</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.subHeader}>
+        <span>보험</span>
+        <img src={LeftChevron} alt="left-chevron" />
+        <span>가입자</span>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.label}>보험</div>
+        <div className={styles.btn}>버튼</div>
+      </div>
+      <div className={styles.dateContainer}>
+        <div className={styles.date}>
+          <span>보험 가입일</span>
+          <span>2024년 1월 1일</span>
+        </div>
+        <div className={styles.date}>
+          <span>다음 갱신일</span>
+          <span>2025년 1월 1일</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Insurance;
