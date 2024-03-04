9c3b4aaf2a382a58ae8764f62f0e0531dcc7a4ca
hyunn522 <tjgus52213@gmail.com>
2024-03-04 10:53:35 +0900

    chore: pages 폴더 추가 및 라우팅 추가 - /insurance 경로 추가

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from '@/Home';
 import { Layout } from '@/layouts/Layout';
 
+import Insurance from './pages/Insurance';
+
 export const Router = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/drone" element={<Home />} />
+        <Route path="/drone" element={<Home />} />,
+        <Route path="/insurance" element={<Insurance />} />
       </Route>
     </Routes>
   );
