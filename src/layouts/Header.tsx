e6efba5ebc0528ae6d4ed32d05db2a9b323ca60b
aaminha <dks4857@gmail.com>
2024-03-04 22:27:31 +0900

    feat: 헤더 구현

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,3 +1,24 @@
+import IconMore from '@/assets/icons/more.svg';
+
+import styles from './Header.module.scss';
+
+const Dummy = {
+  image: null,
+  user: '파블로 항공',
+};
+
 export const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.userInfo}>
+        {Dummy.image ? (
+          <img className={styles.profile} src={Dummy.image} alt="profile" />
+        ) : (
+          <div className={styles.profile} />
+        )}
+        <span>{Dummy.user}</span>
+      </div>
+      <img className={styles.moreButton} src={IconMore} alt="more" />
+    </header>
+  );
 };
