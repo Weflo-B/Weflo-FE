36b97058bbe2804aac3bd51014a2039a0ec82240
hyunn522 <tjgus52213@gmail.com>
2024-03-04 16:32:24 +0900

    feat: insurance scsss 모듈 파일 생성 및 메인 레이아웃 컴포넌트 추가

--- src/pages/Insurance/Insurance.tsx
+++ src/pages/Insurance/Insurance.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// import styles from '@/pages/Insurance/Insurance.module.scss';
+
 const Insurance = () => {
   return <div>Insurance</div>;
 };
--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from '@/Home';
 import { Layout } from '@/layouts/Layout';
 
-import Insurance from './pages/Insurance';
+import Insurance from './pages/Insurance/Insurance';
 
 export const Router = () => {
   return (
