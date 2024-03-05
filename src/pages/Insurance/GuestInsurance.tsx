513df6682efbbdd63d23fcd2e4e8f8e75f91da6a
hyunn522 <tjgus52213@gmail.com>
2024-03-05 12:43:00 +0900

    feat: GuestInsurance 라우팅 추가

---
+++ src/pages/Insurance/GuestInsurance.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from '@/pages/Insurance/GuestInsurance.module.scss';
+
+const GuestInsurance = () => {
+  return <div>GuestInsurance</div>;
+};
+
+export default GuestInsurance;
--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from '@/layouts/Layout';
 import { Tracking } from '@/pages/Tracking';
 
-import Insurance from './pages/Insurance/Insurance';
+// ui 확인 위해 임시 라우팅 -> 로그인에 따른 처리는 나중에 할 예정
+// import Insurance from './pages/Insurance/Insurance';
+import GuestInsurance from './pages/Insurance/GuestInsurance';
 
 export const Router = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/insurance" element={<Insurance />} />
+        {/* <Route path="/insurance" element={<Insurance />} /> */}
+        <Route path="/insurance" element={<GuestInsurance />} />
         <Route path="/tracking" element={<Tracking />} />
       </Route>
     </Routes>
