9f5749d3a8c1fc55c5d959cbfe68224fe12bd210
hyunn522 <tjgus52213@gmail.com>
2024-03-06 22:30:29 +0900

    feat: 부품 주문 헤더 레이아웃 작성

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 
+import styles from './Order.module.scss';
+
 const Order = () => {
   return (
-    <div>
-      Order
+    <div className={styles.container}>
+      <div className={styles.subHeader}>
+        <span>부품 주문</span>
+      </div>
+      <div className={styles.header}>부품 주문</div>
+      <div className={styles.content}></div>
       <Outlet />
     </div>
   );
