f9c750fc7fd8e15a2375b4c4166fba1cb125e09e
aaminha <dks4857@gmail.com>
2024-03-07 06:14:30 +0900

    feat: 주문/배송 상세 조회 페이지에서 버튼 클릭 시 다이얼로그가 뜨도록 구현

--- src/pages/TrackingDetail/index.tsx
+++ src/pages/TrackingDetail/index.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
+import IconCancel from '@/assets/icons/dialog/cancel.svg';
+import IconCompleted from '@/assets/icons/dialog/completed.svg';
 import Button from '@/components/Button';
+import { DefaultDialog } from '@/components/Dialog/DefaultDialog';
 import { ProductInformation } from '@/pages/TrackingDetail/atoms/ProductInformation';
 import { TrackingStep } from '@/pages/TrackingDetail/atoms/TrackingStep';
 
@@ -14,32 +19,59 @@ const Dummy = {
   description: 'DJI 아바타 드론 블레이드 프로펠러 교체용 경량 날개 팬 프로펠러 액세서리',
   count: 1,
   price: 7500,
-  step: 'start',
+  step: 'arrived',
 };
 
 export const TrackingDetail = () => {
+  const [activeCancelModal, setActiveCancelModal] = useState(false);
+  const [activeCompletedModal, setActiveCompletedModal] = useState(false);
+
   return (
-    <main className={styles.container}>
-      <div className={styles.titleContainer}>
-        <span className={styles.pageDescription}>주문/배송조회</span>
-        <div className={styles.titleInnerContainer}>
-          <span className={styles.title}>주문 / 배송조회</span>
+    <>
+      <main className={styles.container}>
+        <div className={styles.titleContainer}>
+          <span className={styles.pageDescription}>주문/배송조회</span>
+          <div className={styles.titleInnerContainer}>
+            <span className={styles.title}>주문 / 배송조회</span>
+          </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <ProductInformation product={Dummy} />
-        <TrackingStep product={Dummy} />
-      </div>
-      {Dummy.step === 'prepare' && (
-        <Button style={{ position: 'absolute', bottom: '80px', left: '1565px' }}>
-          주문 취소하기
-        </Button>
+        <div className={styles.content}>
+          <ProductInformation product={Dummy} />
+          <TrackingStep product={Dummy} />
+        </div>
+        {Dummy.step === 'prepare' && (
+          <Button
+            style={{ position: 'absolute', bottom: '80px', left: '1565px' }}
+            onClick={() => setActiveCancelModal(true)}
+          >
+            주문 취소하기
+          </Button>
+        )}
+        {Dummy.step === 'arrived' && (
+          <Button
+            style={{ position: 'absolute', bottom: '80px', left: '1565px' }}
+            onClick={() => setActiveCompletedModal(true)}
+          >
+            교환/반품하기
+          </Button>
+        )}
+      </main>
+      {activeCancelModal && (
+        <DefaultDialog
+          icon={<img src={IconCancel} alt="icon" />}
+          text="주문이 취소되었습니다!"
+          open={activeCancelModal}
+          onClick={() => setActiveCancelModal(false)}
+        />
       )}
-      {Dummy.step === 'arrived' && (
-        <Button style={{ position: 'absolute', bottom: '80px', left: '1565px' }}>
-          교환/반품하기
-        </Button>
+      {activeCompletedModal && (
+        <DefaultDialog
+          icon={<img src={IconCompleted} alt="icon" />}
+          text="교환 / 반품 접수가 완료되었어요!"
+          open={activeCompletedModal}
+          onClick={() => setActiveCompletedModal(false)}
+        />
       )}
-    </main>
+    </>
   );
 };
