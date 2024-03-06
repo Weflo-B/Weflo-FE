cf0c249feb7dea861a9cfd099a4131bd16b42544
hyunn522 <tjgus52213@gmail.com>
2024-03-06 23:59:43 +0900

    feat: 부품 주문 드론 프로필 레이아웃 완성

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import DroneImg from '@/assets/icons/droneImg.svg';
+import { PART_ORDER_LABELS } from '@/constants/orderConstants';
 import { DroneData } from '@/types';
 
 import styles from './Order.module.scss';
@@ -43,7 +44,31 @@ const Order = () => {
         <span>부품 주문</span>
       </div>
       <div className={styles.header}>부품 주문</div>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        {Dummy.map((data) => {
+          return (
+            <div className={styles.droneContainer} key={data.name}>
+              <div className={styles.droneHeader}>
+                <div className={styles.droneProfile}>
+                  <img src={data.img} alt="드론 이미지" />
+                  <div>{data.name}</div>
+                </div>
+                <div className={styles.scoreContainer}>
+                  <div className={styles.score}>
+                    <div className={styles.label}>{PART_ORDER_LABELS[0]}</div>
+                    <div className={styles.value}>{data.balanceScore}</div>
+                  </div>
+                  <div className={styles.scoreDivider} />
+                  <div className={styles.score}>
+                    <div className={styles.label}>{PART_ORDER_LABELS[1]}</div>
+                    <div className={styles.value}>{data.totalScore}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <Outlet />
     </div>
   );
