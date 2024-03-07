beb1fe7ac141759cf88d89ef53cfbd7872bc64d2
hyunn522 <tjgus52213@gmail.com>
2024-03-08 00:38:06 +0900

    feat: 부품 상세 드론 점수 레이아웃 구현 - 부품 개수가 3개, 2개, 1개일 때 모두 정상 동작 확인

--- src/pages/Order/atoms/DroneDetailitem.tsx
+++ src/pages/Order/atoms/DroneDetailitem.tsx
@@ -15,13 +15,43 @@ const DroneDetailItem = ({ drone }: DroneDetailItemProps) => {
           <img src={drone.img} alt="드론 이미지" />
         </div>
         <div className={styles.profile}>
-          <div className={styles.info}>
-            <div className={styles.label}>제품명</div>
-            <div className={styles.value}>{drone.name}</div>
+          <div className={styles.infoContainer}>
+            <div className={styles.info}>
+              <div className={styles.label}>제품명</div>
+              <div className={styles.value}>{drone.name}</div>
+            </div>
+            <div className={styles.info}>
+              <div className={styles.label}>주문일</div>
+              <div className={styles.value}>{drone.orderDate}</div>
+            </div>
           </div>
-          <div className={styles.info}>
-            <div className={styles.label}>주문일</div>
-            <div className={styles.value}>{drone.orderDate}</div>
+          <div className={styles.partInfo}>
+            {drone.currentPart.map((part, index) => {
+              const isLast = index === Object(drone.currentPart).length - 1;
+              return (
+                <>
+                  {Object.entries(part).map(([kind, details], index) => {
+                    return (
+                      <div
+                        className={`${styles.partContainer} ${isLast && styles.lastContainer}`}
+                        key={index}
+                      >
+                        <div className={styles.partKind}>{kind}</div>
+                        {details.map((detail) => {
+                          return (
+                            <div className={styles.partDetail}>
+                              <div>
+                                {detail.name} ({detail.score}점)
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    );
+                  })}
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
