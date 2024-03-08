985e758b7cd44649cc98bc2efd3db499076255ed
hyunn522 <tjgus52213@gmail.com>
2024-03-08 21:24:35 +0900

    feat: getInsurance 함수 정의

--- src/services/insuranceApi.ts
+++ src/services/insuranceApi.ts
@@ -1,1 +1,7 @@
 import { HTTP_URL } from '.';
+
+export const getInsruance = async (userId: number) => {
+  const res = await fetch(`${HTTP_URL}//api/insurance/${userId}`);
+  const data = res.json();
+  return data;
+};
