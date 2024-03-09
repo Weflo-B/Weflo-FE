f80f607d5d39c128b48c4951f97a8a3c32dd1bcd
Minha Ahn <dks4857@gmail.com>
2024-03-09 16:02:05 +0900

    [FEAT] 부품 주문 레이아웃 중 견적서 퍼블리싱 구현 (#18)
    * feat: 주문 완료 화면 구현
    
    * feat: 견적서 화면 구현

--- src/pages/Order/OrderEstimate.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const OrderEstimate = () => {
-  return <div>OrderEstimate</div>;
-};
-
-export default OrderEstimate;
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
 
 import { Layout } from '@/layouts/Layout';
+import { Estimate } from '@/pages/Estimate';
 import { Tracking } from '@/pages/Tracking';
 import { TrackingDetail } from '@/pages/TrackingDetail';
 
@@ -8,11 +9,11 @@ import GuestInsurance from './pages/Insurance/GuestInsurance';
 import Insurance from './pages/Insurance/Insurance';
 import Order from './pages/Order';
 import OrderDetail from './pages/Order/OrderDetail';
-import OrderEstimate from './pages/Order/OrderEstimate';
 
 export const Router = () => {
   return (
     <Routes>
+      <Route path="/test" element={<Layout />}></Route>
       <Route element={<Layout />}>
         <Route path="/insurance" element={<Outlet />}>
           <Route index element={<Insurance />} />
@@ -21,7 +22,7 @@ export const Router = () => {
         <Route path="/order" element={<Outlet />}>
           <Route index element={<Order />} />
           <Route path="detail/:droneId" element={<OrderDetail />} />
-          <Route path="estimate" element={<OrderEstimate />} />
+          <Route path="estimate" element={<Estimate />} />
         </Route>
         <Route path="/tracking" element={<Tracking />} />
         <Route path="/tracking/detail" element={<TrackingDetail />} />
