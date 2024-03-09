cbb4d4cef11d6413dc02ec906bfab96960f6f41d
hyunn522 <tjgus52213@gmail.com>
2024-03-10 00:50:49 +0900

    fix: 부품 주문 상세 페이지 상단에 드론 이름 띄우기

--- src/pages/Order/OrderDetail.tsx
+++ src/pages/Order/OrderDetail.tsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from 'react';
+
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import RightChevron from '@/assets/icons/rightChevron.svg';
+import { USER_ID } from '@/services';
+import { getDronePart } from '@/services/orderApi';
+import { DronePartDetailData } from '@/types';
 
 import styles from './OrderDetail.module.scss';
 import DroneDetailItem from './atoms/DroneDetailItem';
@@ -9,15 +15,28 @@ const OrderDetail = () => {
   const { droneId } = useParams();
   const numDroneId = Number(droneId);
 
+  const [droneData, setDroneData] = useState<DronePartDetailData>();
+
+  const { data } = useQuery({
+    queryKey: ['ORDER_PARTS', 'ALL', USER_ID, numDroneId],
+    queryFn: () => getDronePart(USER_ID, numDroneId),
+  });
+
+  useEffect(() => {
+    if (data) {
+      setDroneData(data.orderParts);
+    }
+  }, [data]);
+
   return (
     <div className={styles.container}>
       <div className={styles.subHeader}>
         <span>부품 주문</span>
         <img src={RightChevron} alt="left-chevron" />
-        <span>드론이름</span> {/* API 연결할 때 드론 데이터 받아오면 이름으로 변경 예정*/}
+        <span>드론이름</span>
       </div>
-      <div className={styles.header}>드론이름</div>
-      <DroneDetailItem droneId={numDroneId} />
+      <div className={styles.header}>{droneData?.nickname}</div>
+      <DroneDetailItem drone={droneData!} />
     </div>
   );
 };
--- src/pages/Order/atoms/DroneDetailItem.tsx
+++ src/pages/Order/atoms/DroneDetailItem.tsx
@@ -1,53 +1,33 @@
-import { useEffect, useState } from 'react';
-
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-
-import { USER_ID } from '@/services';
-import { getDronePart } from '@/services/orderApi';
 import { DronePartDetailData } from '@/types';
 
 import styles from './DroneDetailItem.module.scss';
 import PartOnSale from './PartOnSale';
 
 interface DroneDetailItemProps {
-  droneId: number;
+  drone: DronePartDetailData;
 }
 
-const DroneDetailItem = ({ droneId }: DroneDetailItemProps) => {
-  const [droneData, setDroneData] = useState<DronePartDetailData>();
-
-  const { data } = useQuery({
-    queryKey: ['ORDER_PARTS', 'ALL', USER_ID, droneId],
-    queryFn: () => getDronePart(USER_ID, droneId),
-  });
-
-  useEffect(() => {
-    if (data) {
-      setDroneData(data.orderParts);
-    }
-  }, [data]);
-
+const DroneDetailItem = ({ drone }: DroneDetailItemProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.imgContainer}>
-          <img src={droneData?.droneImg} alt="드론 이미지" />
+          <img src={drone?.droneImg} alt="드론 이미지" />
         </div>
         <div className={styles.profile}>
           <div className={styles.infoContainer}>
             <div className={styles.info}>
               <div className={styles.label}>제품명</div>
-              <div className={styles.value}>{droneData?.nickname}</div>
+              <div className={styles.value}>{drone?.nickname}</div>
             </div>
             <div className={styles.info}>
               <div className={styles.label}>주문일</div>
-              <div className={styles.value}>{droneData?.orderDate}</div>
+              <div className={styles.value}>{drone?.orderDate}</div>
             </div>
           </div>
           <div className={styles.partInfo}>
-            {droneData?.abnormalities.map((data, index) => {
-              const isLast = index === droneData.abnormalities.length - 1;
+            {drone?.abnormalities.map((data, index) => {
+              const isLast = index === drone.abnormalities.length - 1;
               return (
                 <div
                   className={`${styles.partContainer} ${isLast && styles.lastContainer}`}
@@ -70,7 +50,7 @@ const DroneDetailItem = ({ droneId }: DroneDetailItemProps) => {
         </div>
       </div>
       <div className={styles.content}>
-        {droneData?.productsInfo.map((part, index) => {
+        {drone?.productsInfo.map((part, index) => {
           return <PartOnSale part={part} key={index} />;
         })}
       </div>
