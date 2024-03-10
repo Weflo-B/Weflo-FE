fd9fc4faeddd3a600cbccc4963c32269e8926cef
aaminha <dks4857@gmail.com>
2024-03-10 21:41:32 +0900

    fix: 조회 날짜, 결제 날짜를 기준 수정

--- src/pages/Tracking/atoms/OrderInformation/index.tsx
+++ src/pages/Tracking/atoms/OrderInformation/index.tsx
@@ -11,25 +11,23 @@ export const OrderInformation = ({ month, sumPrice }: OrderInformationProps) => 
   return (
     <section className={styles.historyContainer}>
       <div className={styles.title}>
-        {month > today.getMonth() + 1 && (
-          <div className={styles.orderTitle}>{month}월의 결제 내역</div>
-        )}
-        {month === today.getMonth() + 1 && (
+        {month < today.getMonth() && <div className={styles.orderTitle}>{month}월의 결제 내역</div>}
+        {month === today.getMonth() && (
           <div className={styles.orderTitle}>
-            {month}월의 {today.getDate() < 15 ? '결제' : '주문'} 내역
+            {month}월의 {today.getDate() < 15 ? '주문' : '결제'} 내역
           </div>
         )}
-        {month < today.getMonth() + 1 && (
-          <div className={styles.orderTitle}>{month}월의 주문 내역</div>
-        )}
+        {month > today.getMonth() && <div className={styles.orderTitle}>{month}월의 주문 내역</div>}
+
         <div className={styles.orderDescription}>
-          {month > today.getMonth() + 1 && <span className={styles.dueDate}>결제 예정일 | </span>}
-          {month === today.getMonth() + 1 && (
+          {month < today.getMonth() && <span className={styles.dueDate}>결제 완료일 | </span>}
+          {month === today.getMonth() && (
             <span className={styles.dueDate}>
-              {today.getDate() < 15 ? '결제 완료일 | ' : '결제 예정일 | '}
+              {today.getDate() < 15 ? '결제 예정일 | ' : '결제 완료일 | '}
             </span>
           )}
-          {month < today.getMonth() + 1 && <span className={styles.dueDate}>결제 완료일 | </span>}
+          {month > today.getMonth() && <span className={styles.dueDate}>결제 예정일 | </span>}
+
           <span>{`${month + 1 > 12 ? 1 : month + 1}월 ${15}일`}</span>
         </div>
       </div>
