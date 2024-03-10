59a2b1acf64c9179c0844f5fc7bcc5c7609b53e2
aaminha <dks4857@gmail.com>
2024-03-10 14:59:50 +0900

    feat: route에 없는 path는 주문/배송 조회 페이지로 이동하도록 구현

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,17 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import { Layout } from '@/layouts/Layout';
 import { Estimate } from '@/pages/Estimate';
+import GuestInsurance from '@/pages/Insurance/GuestInsurance';
+import Insurance from '@/pages/Insurance/Insurance';
+import Order from '@/pages/Order';
+import OrderDetail from '@/pages/Order/OrderDetail';
 import { Tracking } from '@/pages/Tracking';
 import { TrackingDetail } from '@/pages/TrackingDetail';
-
-import GuestInsurance from './pages/Insurance/GuestInsurance';
-import Insurance from './pages/Insurance/Insurance';
-import Order from './pages/Order';
-import OrderDetail from './pages/Order/OrderDetail';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/test" element={<Layout />}></Route>
       <Route element={<Layout />}>
         <Route path="/insurance" element={<Outlet />}>
           <Route index element={<Insurance />} />
@@ -28,6 +26,7 @@ export const Router = () => {
           <Route index element={<Tracking />} />
           <Route path="detail/:historyId" element={<TrackingDetail />} />
         </Route>
+        <Route path="*" element={<Navigate to="/tracking" />} />
       </Route>
     </Routes>
   );
